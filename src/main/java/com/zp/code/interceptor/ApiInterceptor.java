74dbb737e3566e62564ff89a7eb98e3b60ca9bad
zhangpeng <weidian@bangzhuMacBook-Pro.local>
2020-05-14 10:40:16 +0800

    add cors

--- src/main/java/com/zp/code/interceptor/ApiInterceptor.java
+++ src/main/java/com/zp/code/interceptor/ApiInterceptor.java
@@ -30,6 +30,11 @@ public class ApiInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, *");
+        response.setHeader("P3P", "CP=\"IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA\"");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         if (!(handler instanceof HandlerMethod)) {
             return false;
         }
